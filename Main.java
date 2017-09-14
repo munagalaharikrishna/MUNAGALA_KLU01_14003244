689b11763c42af74386656fe42502b30bf62d823
munagalaharikrishna <31885639+munagalaharikrishna@users.noreply.github.com>
2017-09-14 12:44:16 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package epam.project;
+
+/**
+ *
+ * @author klu
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
+
